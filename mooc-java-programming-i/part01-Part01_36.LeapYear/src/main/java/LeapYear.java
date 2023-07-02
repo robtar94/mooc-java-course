04a8d8097da2d97f75adad4f092605ed4fcdee1a
Robert Tarnas <robert.tarnas@protonmail.com>
2023-07-02 18:26:38 +0200

    part 1 finished

--- mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -1,3 +1,8 @@
+/*
+ * A year is a leap year if it is divisible by 4. 
+ * However, if the year is divisible by 100, 
+ * then it is a leap year only when it is also divisible by 400.
+ */
 
 import java.util.Scanner;
 
@@ -5,6 +10,19 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give a year:");
+        int year = Integer.valueOf(scan.nextLine());
+
+        if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println("The year is a leap year.");
+        } else if (year % 100 == 0 && year % 400 == 0) {
+            System.out.println("The year is a leap year.");
+        } else if (year % 100 == 0 && year % 400 != 0) {
+            System.out.println("The year is not leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
 
     }
 }
--- mooc-java-programming-i/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ mooc-java-programming-i/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -1,10 +1,26 @@
-
 import java.util.Scanner;
 
 public class GiftTax {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Value of the gift?");
+        int gift = Integer.valueOf(scan.nextLine());
+
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        } else if (gift >= 5000 && gift <= 25_000) {
+            System.out.println("Tax: " + (100 + (gift - 5000) * 0.08));
+        } else if (gift >= 25_000 && gift <= 55_000) {
+            System.out.println("Tax: " + (1700 + (gift - 25_000) * 0.10));
+        } else if (gift >= 55_000 && gift <= 200_000) {
+            System.out.println("Tax: " + (4700 + (gift - 55_000) * 0.12));
+        } else if (gift >= 200_000 && gift <= 1_000_000) {
+            System.out.println("Tax: " + (22_100 + (gift - 200_000) * 0.15));
+        } else {
+            System.out.println("Tax: " + (142_100 + (gift - 1_000_000) * 0.17));
+        }
 
     }
 }
