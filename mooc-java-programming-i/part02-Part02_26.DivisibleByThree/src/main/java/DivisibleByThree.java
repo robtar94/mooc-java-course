9d26894b41383574286b17523d3d7cc88de6ad9c
Robert Tarnas <robert.tarnas@protonmail.com>
2023-07-26 11:45:42 +0200

    summation done

--- mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -6,6 +6,16 @@ public class DivisibleByThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        divisibleByThreeInRange(3, 6);
+
+    }
+
+    public static void divisibleByThreeInRange(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if ( i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 
 }
