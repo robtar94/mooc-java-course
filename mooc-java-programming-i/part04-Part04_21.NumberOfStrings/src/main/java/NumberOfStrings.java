3043ae145ecf3196ec6398de0d7bfb3490029693
Robert Tarnas <robert.tarnas@protonmail.com>
2024-01-07 22:36:09 +0100

    Printing a Specified File finished

--- mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -6,5 +6,19 @@ public class NumberOfStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int count = 0;
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            count++;
+
+            if(input.equals("end")) {
+                count--;
+                break;
+            }
+        }
+        System.out.println(count);
+
     }
 }
--- mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -1,4 +1,3 @@
-
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -7,5 +6,18 @@ public class PrintingASpecifiedFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Which file should have its contents printed?");
+        String fileName = scanner.nextLine();
+
+        try (Scanner reader = new Scanner(Paths.get(fileName))) {
+
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+                System.out.println(row);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
